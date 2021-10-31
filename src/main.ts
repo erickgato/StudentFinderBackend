ec67cfeed8c6ecbb038d349be13acd068d6492cf
erickgato <itsbk0703@gmail.com>
2021-10-31 12:31:35 -0300

    chore(deps): adiciona cors e pipeline de validação

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpStatus, ValidationPipe } from '@nestjs/common';
+import { Logger } from 'nestjs-pino';
+import * as helmet from 'helmet';
+
 async function bootstrap() {
   const port = process.env.APP_PORT;
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: /^(.*)/,
+    credentials: true,
+    allowedHeaders: '*',
+    preflightContinue: false,
+    optionsSuccessStatus: HttpStatus.OK,
+  });
+  app.useLogger(app.get(Logger));
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.use(helmet());
   await app.listen(port);
 
   console.log('App:: listen on  port', port);
