22eaa9801c6b61f8e5bb3febdca2f1c52b116f78
erickgato <itsbk0703@gmail.com>
2021-10-31 12:22:32 -0300

    chore(docker): adiciona docker

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
 async function bootstrap() {
+  const port = process.env.APP_PORT;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
+
+  console.log('App:: listen on  port', port);
 }
 bootstrap();
